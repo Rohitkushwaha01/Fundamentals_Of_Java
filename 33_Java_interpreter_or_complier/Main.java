564d7f1fe09b0ab0e9662eff99acc03796fa8e6a
Rohit Kushwaha <97441699+Rohitkushwaha01@users.noreply.github.com>
2022-08-02 22:53:54 +0530

    Add files via upload

---
+++ 33_Java_interpreter_or_complier/Main.java
@@ -0,0 +1,13 @@
+// Interpreter = Interpreter translate one statement at a time into machine.
+// Partial execution if error.
+// Easy for programmers.
+
+
+// Compiler = Compiler scans the entire program and translate whole of it into machine code.
+// No execution if an error occurs.
+// usually not as easy.
+
+
+
+// Java is a hybrid language --> Both compiled and interpreted.
+// Java file is first compiled and generates a class file. This class file consist of bytecode which makes java an platform independent language. using this class file we can use any Operating system to run this code. And the execution of this bytecode is done by using interpreter.

435e3c9e0413d9b53bd9133418f977ffa34ea1c2
Rohit Kushwaha <97441699+Rohitkushwaha01@users.noreply.github.com>
2022-08-13 23:47:15 +0530

    Add files via upload

---
+++ Practice/Conditions/01_marks/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks of English: ");
+        int English = sc.nextInt();
+
+        System.out.print("Enter marks of Maths: ");
+        int Maths = sc.nextInt();
+
+        System.out.print("Enter marks of Science: ");
+        int Science = sc.nextInt();
+
+        int total = English + Maths + Science;
+        System.out.println(total);
+
+        float percentage = (float)(total*100)/300;
+
+        System.out.println(percentage);
+
+        if(English>=33 && Maths>=33 && Science>=33){
+            if(percentage >= 40){
+                System.out.println("Congratulation!! You're passed with " + percentage + "%");
+            }
+            else{
+                System.out.println("Sorry! You're not passed" + "You're percentage is " + percentage + "%");
+            }
+        }
+        else{
+            System.out.println("Sorry!! You're failed in one or more of the subjects");
+        }
+
+    }
+}

7d662c22a02e233fa360505085052d5d7d8a1713
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-11-05 17:42:59 +0530

    Brute Force approach and Prefix Sum Approach

--- DSA_Java/Array/09_PrefixSum/Main.java
+++ DSA_Java/Array/09_PrefixSum/Main.java
@@ -1,14 +1,20 @@
-// Brute Force Approach: O(N^3)
+// Prefix Sum approach : O(N^2)
 
 public class Main {
     public static int largestSubarray(int array[], int n){
+        int prefix[] = new int[100]; 
+        prefix[0] = array[0];
+        System.out.print(prefix[0] + ", ");
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + array[i];
+            System.out.print(prefix[i] + ", ");
+        }
+
+
         int largestArray = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                int subArraySum = 0;
-                for (int k = i; k <= j; k++) {
-                    subArraySum += array[k];
-                }
+                int subArraySum = (i > 0) ? prefix[j] - prefix[i - 1] : prefix[j];
                 if(largestArray > subArraySum){
                     largestArray = largestArray;
                 }

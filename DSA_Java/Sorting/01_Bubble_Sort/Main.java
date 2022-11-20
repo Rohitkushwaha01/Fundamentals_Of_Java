6b28abd804e37e1e083b8c413ceea8858f000284
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-11-20 22:41:11 +0530

    bubble sort and insertion sor

--- DSA_Java/Sorting/01_Bubble_Sort/Main.java
+++ DSA_Java/Sorting/01_Bubble_Sort/Main.java
@@ -1,21 +1,24 @@
 // Bubble sort : Take larger element to the end of the array by reapeatedly swapping the adjacent elements.
 public class Main{
     public static void bubbleSort(int array[]){
-        for (int i = 1; i < array.length; i++) {
-            for (int j = 0; j < array.length-i; j++) {
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1-i; j++) {
                 if(array[j] > array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] =temp;
+                    System.out.println(j);
                 }
             }
         }
     }
     public static void main(String[] args) {
-        int arr[] = {5, 4, 3, 2, 1, -4, 3, 0, -1};
+        int arr[] = {5, 4, 3, 2};
         bubbleSort(arr);
+        System.out.print("[ ");
         for (int i : arr) {
-            System.out.print(i);
+            System.out.print(i + ", ");
         }
+        System.out.print("]");
     }
 }
---
+++ DSA_Java/Sorting/02_insertion_sort/Main.java
@@ -0,0 +1,5 @@
+// Insertion sort: Insertion sort is similar to playing cards in our hands. Insert the card in its correct position in a sorted part.
+
+public class Main {
+    
+}

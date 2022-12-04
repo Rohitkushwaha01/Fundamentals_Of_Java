956f0c654b8820c8b8c910fa8113c35a75a167f4
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-04 23:15:06 +0530

    Permutation

---
+++ DSA_Java/03_Character_Array/06_Array_permutation/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static int permutation(String A, String B) {
+        A = A.toLowerCase();
+        B = B.toLowerCase();
+
+        int len = (A.length() > B.length())?A.length():B.length();
+        int matched = 0;
+        for (int i = 0; i < A.length(); i++) {
+            char word = A.charAt(i);
+            for (int j = 0; j < B.length(); j++) {
+                if(word == B.charAt(j)){
+                    matched++;
+                }
+            }
+        }
+        if(matched == len){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
+        int value = permutation("abcd", "dacb");
+        if(value == 1){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}

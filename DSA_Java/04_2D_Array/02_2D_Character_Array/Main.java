a27fd855cc0a290c26988fd74055187e013f91fc
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-07 05:29:23 +0530

    String 2D Array

---
+++ DSA_Java/04_2D_Array/02_2D_Character_Array/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void printArray(int numbersArray[][], int r, int c){
+        
+    }
+    public static void main(String[] args) {
+        String numbers[][] = {
+            {"One", "Two", "Three"},
+            {"Four", "Five", "Six"}
+        };
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(numbers[i][j]);
+            }
+        }
+
+    }
+}

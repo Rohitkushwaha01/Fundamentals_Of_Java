3f1446a5c5630d36a0bc8a64ffe938c8ca16b7e3
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-12 04:46:47 +0530

    bitwise operators

---
+++ DSA_Java/05_Bitwise/01_Bitwise_Operators/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(5&7); // Bitwise And operator
+        System.out.println(5|8); // Bitwise OR operator
+        System.out.println(5^7); // Bitwise XOR operator
+        System.out.println(~0); // Bitwise NOT operator
+        System.out.println(5<<2); // Bitwise left shift operator
+        System.out.println(10>>1); // Bitwise right shift operator
+    }
+}

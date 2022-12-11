02e4225d78341053316f7164c80e50300847084f
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-12 04:53:50 +0530

    ODD EVEN

--- DSA_Java/05_Bitwise/02_Odd_Even/Main.java
+++ DSA_Java/05_Bitwise/02_Odd_Even/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
         if((a&1)==1){
             System.out.println("ODD");
         }

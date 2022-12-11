f538ad148a1c9ec20b05e462904e38abf23e71b1
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-12 04:52:28 +0530

    bitwise operators

---
+++ DSA_Java/05_Bitwise/02_Odd_Even/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        if((a&1)==1){
+            System.out.println("ODD");
+        }
+        else{
+            System.out.println("EVEN");
+        }
+    }
+}

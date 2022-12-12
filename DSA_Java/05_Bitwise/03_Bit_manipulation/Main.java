e9dc23f7e858a3804e6c1a2c88366d0d11148a7b
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-12 05:33:08 +0530

    clear ith bit

--- DSA_Java/05_Bitwise/03_Bit_manipulation/Main.java
+++ DSA_Java/05_Bitwise/03_Bit_manipulation/Main.java
@@ -12,12 +12,19 @@ public class Main {
         return n;
     }
 
+    public static int clearithBit(int n, int i){
+        int mask = ~(1 << i);
+        n = (n&mask);
+        return n;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = 10;
         int position = sc.nextInt();
         // System.out.println(getIthBit(number, position));
-        number = setithBit(number, position);
+        // number = setithBit(number, position);
+        number = clearithBit(number, position);
         System.out.println(number);
     }
 }

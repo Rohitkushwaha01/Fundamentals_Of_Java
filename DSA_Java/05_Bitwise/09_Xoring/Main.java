a5fd9afd2c648f61678d7a933602c2ca783540d6
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-14 21:01:37 +0530

    Xoring

---
+++ DSA_Java/05_Bitwise/09_Xoring/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main{
+    public static int Xoring(ArrayList<Integer> v){
+        int res=0;
+        for(int x : v){
+            res ^= x;
+            System.out.println(res);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(1);
+        arr.add(4);
+        arr.add(2);
+        arr.add(3);
+
+        System.out.println(Xoring(arr));
+    }
+}

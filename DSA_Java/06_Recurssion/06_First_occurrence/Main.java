aac2d0bd11fd2952b56bf13e283a447321196269
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-18 23:10:15 +0530

    First occurrence another code

--- DSA_Java/06_Recurssion/06_First_occurrence/Main.java
+++ DSA_Java/06_Recurssion/06_First_occurrence/Main.java
@@ -13,10 +13,28 @@ public class Main {
         }
     }
 
+    static int firstOcc(int i, int arr[],int n,int key){
+
+        //base case
+        if(i == n){
+            return -1;
+        }
+        //rec case
+        if(arr[i]==key){
+            return 0;
+        }
+        int subIndex = firstOcc(i+1, arr, n, key);
+        if(subIndex!=-1){
+            return subIndex + 1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = { -2, 3, 4, -1, 5, -12, 6, 1, 3 };
-
-
-        first(arr, -1, 0);
+        int n = arr.length;
+        int key = 6;
+        // first(arr, -1, 0);
+        System.out.println((firstOcc(0,arr,n,key))); 
     }
 }

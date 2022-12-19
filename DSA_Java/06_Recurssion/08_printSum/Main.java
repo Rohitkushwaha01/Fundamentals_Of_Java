8d9dfbf77e5a25ea9d92a1053236843ead61cd99
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-12-19 19:01:40 +0530

    print first n natural number

---
+++ DSA_Java/06_Recurssion/08_printSum/Main.java
@@ -0,0 +1,19 @@
+// Print sum of first n natural numbers.
+// e.g = print sum of first 5 numbers
+// input = 5;
+// result = 1+2+3+4+5 = 15;
+
+public class Main {
+    public static void printSum(int i, int n, int sum) {
+        if(i == n){
+            sum = sum + i;
+            System.out.println(sum);
+            return;
+        }
+        sum = sum + i;
+        printSum(i+1, n, sum);
+    }
+    public static void main(String[] args) {
+        printSum(1, 10, 0);
+    }
+}

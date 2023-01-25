28cf6e1a22550de43a125e612f854a29a6b24bfa
unknown <rohitkushwahac@gmail.com>
2023-01-25 06:11:44 +0530

    pattern 02

--- DSA_Java/08_pattern/01_pattern/Main.java
+++ DSA_Java/08_pattern/01_pattern/Main.java
@@ -7,9 +7,9 @@
 
 public class Main {
     public static void main(String[] args){
-        for(int i = 0; i < 5; i++){
-            for(int j = 1; j < i; j++){
-                System.out.println(j);
+        for(int i = 0; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }

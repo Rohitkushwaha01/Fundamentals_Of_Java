c5f0be7ef8ac2491cbabb7a5ed8126df7d440ad3
unknown <rohitkushwahac@gmail.com>
2023-02-05 11:37:29 +0530

    Swap variables with and without

---
+++ DSA_Java/01_Array/18_SwapArraysElement/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+// Given an array and two indexes of array swap that indexes element
+// Input => arr =  [10, 45, 32, 53, 90] index1 = 2, index2 = 4
+// Output => arr = [10, 45, 90, 53, 32]
+
+public class Main {
+    // Swapping with using one extra variable
+    public static void swapWithVariable(int arr[], int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+
+    // Swapping without using one extra variable(= & -)
+    public static void swapWithoutVariable(int arr[], int idx1, int idx2) {
+        arr[idx1] = arr[idx1] + arr[idx2];
+        arr[idx2] = arr[idx1] - arr[idx2];
+        arr[idx1] = arr[idx1] - arr[idx2];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 45, 32, 53, 90};
+        int index1 = 2;
+        int index2 = 4;
+        swapWithVariable(arr, index1, index2);
+        System.out.println(Arrays.toString(arr));
+        swapWithoutVariable(arr, index1, index2);
+        System.out.println(Arrays.toString(arr));
+    }
+}

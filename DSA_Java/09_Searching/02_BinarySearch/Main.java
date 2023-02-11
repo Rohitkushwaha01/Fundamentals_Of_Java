c91c093fde1d64b3777feea1d968ea8dd5fc1850
unknown <rohitkushwahac@gmail.com>
2023-02-11 23:30:31 +0530

    binary search

--- DSA_Java/09_Searching/02_BinarySearch/Main.java
+++ DSA_Java/09_Searching/02_BinarySearch/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3,4,56,345,600,500};
-        int target = 6;
+        int[] arr = {3,4,56,345,600,700};
+        int target = 345;
         int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
@@ -9,16 +9,16 @@ public class Main {
     public static int binarySearch(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
-        while (end>start) {
-            int mid = (start + end)/2;
-            if(arr[mid] == target ){
-                return mid;
+        while (end>=start) {
+            int mid = start + (end-start)/2;
+            if(arr[mid] > target ){
+                end = mid-1;
             }
-            if(arr[mid] > target){
-                end = mid;
+            else if(arr[mid]<target){
+                start = mid+1;
             }
             else{
-                start = mid;
+                return mid;
             }
         }
 

d2fe19954913dd7b09d1994f624cfaff638fdc4e
unknown <rohitkushwahac@gmail.com>
2023-02-12 10:49:18 +0530

    bubble sort optimised

--- DSA_Java/02_Sorting/01_bubble_sort/Main.java
+++ DSA_Java/02_Sorting/01_bubble_sort/Main.java
@@ -1,24 +1,27 @@
+import java.util.Arrays;
+
 // Bubble sort : Take larger element to the end of the array by reapeatedly swapping the adjacent elements.
 public class Main{
     public static void bubbleSort(int array[]){
+        boolean swapped = false;
         for (int i = 0; i < array.length-1; i++) {
             for (int j = 0; j < array.length-1-i; j++) {
                 if(array[j] > array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] =temp;
-                    System.out.println(j);
+                    swapped = true;
                 }
             }
+            if(!swapped){
+                break;
+            }
         }
+
     }
     public static void main(String[] args) {
-        int arr[] = {5, 4, 3, 2};
+        int arr[] = {1,2,3,4};
         bubbleSort(arr);
-        System.out.print("[ ");
-        for (int i : arr) {
-            System.out.print(i + ", ");
-        }
-        System.out.print("]");
+        System.out.print(Arrays.toString(arr));
     }
 }

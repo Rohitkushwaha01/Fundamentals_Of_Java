3736a084a56ed7d873e83e53904c7dd957f125bc
unknown <rohitkushwahac@gmail.com>
2023-02-12 14:02:35 +0530

    ceiling and floor

---
+++ DSA_Java/09_Searching/11_floor/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,3,6,8,9,13,46,89,90};
+        int ans = ceiling(nums, 10);
+        System.out.println(ans);
+    }
+
+    public static int ceiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (end>=start) {
+            int mid = start + (end-start)/2;
+            if(arr[mid] > target ){
+                end = mid - 1;
+            }
+            else if(arr[mid]<target){
+                start = mid+1;
+            }
+            else{
+                return arr[mid];
+            }
+        }
+
+        return arr[end];
+    }
+}

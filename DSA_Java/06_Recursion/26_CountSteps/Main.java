c0eac0a1b991ad94c161f9152a1f5fe6093f7e16
Rohit Kushwaha <97441699+Rohitkushwaha01@users.noreply.github.com>
2023-07-19 23:28:09 +0530

    Create Main.java

---
+++ DSA_Java/06_Recursion/26_CountSteps/Main.java
@@ -0,0 +1,20 @@
+package recursionProblems;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ans = numberOfSteps(14);
+		System.out.print(ans);
+	}
+
+	public static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+
+    public static int helper(int num, int steps){
+        if(num == 0) return steps;
+
+        if(num%2 == 0) return helper(num/2, ++steps);
+        else return helper(num-1, ++steps);
+    }
+}
